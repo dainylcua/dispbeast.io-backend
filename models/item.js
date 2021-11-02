1cc7ddeb6b0ba9948ef6c5b7bd4544f79f7ca93b
Dainyl Cua <dainylcua@gmail.com>
2021-11-02 13:58:32 -0500

    Removes damageSchema

--- models/item.js
+++ models/item.js
@@ -4,18 +4,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const damageSchema = Schema ({
-    quantity: { type: Number },
-    dice: { type: Number },
-    type: { type: String }
-}, { timestamps: true })
-
 const itemSchema = Schema({
     name: { type: String, required: true },
     itemType: { type: String, required: true },
     rarity: { type: String, required: true },
     ac: { type: Number, default: 0 },
-    damage: { type: damageSchema, default: {} },
+    damage: { 
+            quantity: { type: Number, default: 0 },
+            dice: { type: Number, default: 0 },
+            type: { type: String, default: '' }
+        },
     weight: { type: Number, required: true, default: 1 },
     owner: { type: Schema.Types.ObjectId, ref: "User" }
 }, { timestamps: true })
